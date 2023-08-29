bb7f320514589b2e192fd9096a63257fa2f8b914
Chris Spivey <theori373@gmail.com>
2023-08-28 20:37:19 -0700

    updated contact

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -6,6 +6,8 @@ export default function Contact() {
     return (
     <div>
         <h2>Contact Us!😁</h2>
+        <h3>What is your name?</h3>
+        <input className="userName" placeholder="Beepus Leepus"/>
         <h3>Please enter your email</h3>
         <input className="email"/>
         <h3>Message</h3>
