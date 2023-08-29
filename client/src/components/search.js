4e5ae0de44321ff840c82663caac3c335f0df4f1
SarahHawkins26 <sarahhawkins26@gmail.com>
2023-08-28 20:04:42 -0700

    search.js started

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,11 +1,40 @@
-import React from "react";
-import {useState} from "react"
+import React, { useState, useEffect } from "react";
+import { fetchAnime } from "../utils/api";
 
+export default function Search() {
+  const [searchAnime, setSearchAnime] = useState("");
+  const [animeData, setAnimeData] = useState([]);
 
-export default function search() {
-    return (
-        <div>
-            <input placeholder="Attack on Titan"></input>
-        </div>
-    )
+  useEffect(() => {
+    if (searchAnime) {
+      fetchAnime(searchAnime)
+        .then((data) => setAnimeData([data]))
+        .catch((error) => console.error("Error fetching data", error));
+    }
+  }, [searchAnime]);
+
+  const handleSearchChange = (e) => {
+    setSearchAnime(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search for Anime!"
+        value={searchAnime}
+        onChange={handleSearchChange}
+      />
+      <ul>
+        {animeData.map((anime) => (
+          <li key={anime.title}>
+            <img src={anime.imageURL} alt={anime.title} />
+            <h3>{anime.title}</h3>
+            <p>Episodes: {anime.episodes}</p>
+            <p>Status: {anime.status}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
