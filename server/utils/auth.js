ba714f092b9708c6279f1c0a16d01f688ec78314
Chris Spivey <theori373@gmail.com>
2023-09-05 16:52:12 -0700

    fixed mutations and added dotenv

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
-const secret = process.env.secret;
+const secret = process.env.JWTSECRET;
 const expiration = '2h';
 
 module.exports = {
