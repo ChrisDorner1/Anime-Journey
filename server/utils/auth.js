822f64f892634535acf159f30e217521a678e28d
Chris Spivey <theori373@gmail.com>
2023-09-05 17:02:21 -0700

    fixed signToken

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -21,9 +21,12 @@ module.exports = {
         }
         return req;
     },
-    signToken: function ({ firstName, email, _id }) {
-        const payload = { firstName, email, _id };
-      
-        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });      
+    signToken: function (user) {
+        const payload = {userId: user._id, username: user.username, email: user.email };
+        if(!secret) { 
+            throw new Error("JWT secret key not defined")
+        }
+        const token =jwt.sign(payload, secret, {expiresIn: expiration})
+        return token;      
     },
 };
